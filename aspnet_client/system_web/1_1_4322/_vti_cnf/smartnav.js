a45346ee7a98a7bc4578276a565a9fe8242a88e0
Tim Dysinger <tim@dysinger.net>
2014-07-30 13:22:04 -1000

    old kauaiarc.org

---
+++ aspnet_client/system_web/1_1_4322/_vti_cnf/smartnav.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|23 May 2006 07:23:49 -0000
+vti_extenderversion:SR|5.0.2.6790
+vti_lineageid:SR|{6B183E08-C13D-4781-B480-A0F1221680C8}
+vti_cacheddtm:TX|23 May 2006 07:23:49 -0000
+vti_filesize:IR|8571
+vti_backlinkinfo:VX|
---
+++ aspnet_client/system_web/1_1_4322/_vti_cnf/webuivalidation.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|23 May 2006 07:23:50 -0000
+vti_extenderversion:SR|5.0.2.6790
+vti_lineageid:SR|{FDB0EB6B-71C4-4F47-9306-D476E8EDCB85}
+vti_cacheddtm:TX|23 May 2006 07:23:50 -0000
+vti_filesize:IR|14482
+vti_backlinkinfo:VX|
