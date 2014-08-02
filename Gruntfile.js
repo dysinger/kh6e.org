7c23ed01ef9c1a0b424d1796cc3f3188f43ecd6e
Tim Dysinger <tim@dysinger.net>
2014-08-01 15:45:37 -1000

    load-grunt-tasks handles all this for us

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,19 @@
 
 module.exports = function(grunt) {
   require("time-grunt")(grunt);
+  require('load-grunt-tasks')(grunt, {
+    pattern: [
+      'grunt-*',
+      '!grunt-configure',
+      'assemble*',
+      '!assemble-contrib*',
+      '!assemble-middleware*'
+    ]
+  });
 
   grunt.initConfig(
     require("grunt-configure")("./config/*.{js,json,yml}")
   );
-
-  grunt.loadNpmTasks("assemble");
-  grunt.loadNpmTasks("assemble-less");
-  grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-contrib-connect");
-  grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-execute");
-  grunt.loadNpmTasks("grunt-frep");
-  grunt.loadNpmTasks("grunt-purescript");
-  grunt.loadNpmTasks("grunt-sync-pkg");
-  grunt.loadNpmTasks("grunt-s3");
-  grunt.loadNpmTasks("grunt-verb");
 
   grunt.registerTask("build", [
     "psc:lib",
